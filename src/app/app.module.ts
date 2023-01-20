7e8a5e65aefdd5ac9691f771f3b16565e9231e0c
Diego Rodrigues de Sousa <diegors90@gmail.com>
2023-01-20 08:40:25 -0300

    adiciona componente de novo produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NovoProdutoComponent } from "./novo-produto/novo-produto.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NovoProdutoComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        
+    ]
 })
 export class AppModule { }
